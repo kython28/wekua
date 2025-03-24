70cdfdea189933023e41c9c981980b7f3a37c62f
Enrique Mora <kike28.py@protonmail.ch>
2025-03-24 15:37:50 +0400

    refactor: Restructure kernel management and improve type safety in OpenCL kernel compilation

--- src/core/kernel.zig
+++ src/core/kernel.zig
@@ -1,149 +1,197 @@
+const builtin = @import("builtin");
 const std = @import("std");
 const cl = @import("opencl");
 
-const wCommandQueue = @import("command_queue.zig").wCommandQueue;
-const dtypes = @import("../tensor/utils/dtypes.zig");
-const wTensor = dtypes.wTensor;
-const wTensorDtype = dtypes.wTensorDtype;
+const core = @import("main.zig");
+const w_tensor = @import("../tensor/main.zig");
+
+const CommandQueue = core.CommandQueue;
+const Tensor = w_tensor.Tensor;
 
 const header_content: []const u8 = @embedFile("wekua_cl_lib.h");
 
-pub const wKernelsID = enum (u16) {
+pub const KernelsID = enum(u16) {
     Random = 0,
     RandRange = 1,
     Transpose = 2,
     ToComplex = 3,
     ToReal = 4,
-
     AXPY = 5,
-    GEMM = 6
+    GEMM = 6,
 };
 
-pub const total_number_of_kernels: u16 = @intCast(@typeInfo(wKernelsID).@"enum".fields.len);
-pub const total_number_of_headers: u16 = @as(u16, @intCast(@typeInfo(wTensorDtype).@"enum".fields.len)) * 2 * 2;
+pub const total_number_of_kernels = @typeInfo(KernelsID).@"enum".fields.len;
+pub const total_number_of_headers = w_tensor.SupportedTypes.len * 2 * 2;
 
-pub const _w_kernel = struct {
-    kernels: ?[]?cl.kernel.cl_kernel,
-    programs: ?[]?cl.program.cl_program
-};
+kernels: []?cl.kernel.cl_kernel,
+programs: []?cl.program.cl_program,
+initialized: bool = false,
 
-pub const wKernel = *_w_kernel;
-
-pub const wCompileOptions = struct {
-    dtype: wTensorDtype,
+pub const CompileOptions = struct {
     is_complex: bool = false,
     vectors_enabled: bool = true,
     kernel_name: []const u8,
-    extra_args: ?[]const u8 = null
+    extra_args: ?[]const u8 = null,
 };
 
 fn compile_header(
-    command_queue: wCommandQueue, dtype: wTensorDtype, vectors_enabled: bool,
-    is_complex: bool, _: []const u8
+    comptime T: type,
+    command_queue: *const CommandQueue,
+    vectors_enabled: bool,
+    is_complex: bool,
 ) !cl.program.cl_program {
-    const headers = command_queue.headers;
-    const index: u16 = @intFromEnum(dtype) * 4 + @as(u16, @intFromBool(vectors_enabled)) * 2 + @intFromBool(is_complex);
+    const type_index: u16 = w_tensor.getTypeId(T);
+    const index: u16 = type_index * 4 + @as(u16, @intFromBool(vectors_enabled)) * 2 + @intFromBool(is_complex);
 
-    if (headers.programs.?[index]) |prg| {
+    const headers = &command_queue.headers;
+    const programs = headers.programs;
+    if (programs[index]) |prg| {
         return prg;
     }
 
-    // const content: []const u8 = header_content; // Don't ask, this just work and i don't know why
     const new_header_prg = try cl.program.create_with_source(
-        command_queue.ctx.ctx, @as([*]const []const u8, @ptrCast(&header_content))[0..1],
-        command_queue.allocator
+        command_queue.ctx.ctx,
+        @as([*]const []const u8, @ptrCast(&header_content))[0..1],
+        command_queue.allocator,
     );
 
-    headers.programs.?[index] = new_header_prg;
+    programs[index] = new_header_prg;
     return new_header_prg;
 }
 
-fn show_build_log(program: cl.program.cl_program, command_queue: wCommandQueue) !void {
+fn show_build_log(program: cl.program.cl_program, command_queue: *const CommandQueue) !void {
     var msg_len: usize = undefined;
     try cl.program.get_build_info(
-        program, command_queue.device, .build_log, 0, null, &msg_len
+        program,
+        command_queue.device,
+        .build_log,
+        0,
+        null,
+        &msg_len,
     );
 
     const compile_log: []u8 = try command_queue.allocator.alloc(u8, msg_len);
     defer command_queue.allocator.free(compile_log);
 
     try cl.program.get_build_info(
-        program, command_queue.device, .build_log, msg_len, compile_log.ptr, null
+        program,
+        command_queue.device,
+        .build_log,
+        msg_len,
+        compile_log.ptr,
+        null,
     );
 
-    std.log.warn("{s}", .{compile_log});
+    if (builtin.is_test) {
+        std.log.warn("An error while compiling the kernel: {s}", .{compile_log});
+    } else {
+        std.log.err("An error while compiling the kernel: {s}", .{compile_log});
+    }
 }
 
 pub fn compile_kernel(
-    command_queue: wCommandQueue,
-    options: wCompileOptions,
+    comptime T: type,
+    command_queue: *const CommandQueue,
+    options: CompileOptions,
     kernel: *cl.kernel.cl_kernel,
     program: *cl.program.cl_program,
-    content: []const u8
+    content: []const u8,
 ) !void {
     const cl_ctx = command_queue.ctx.ctx;
     const allocator = command_queue.allocator;
     const new_program = try cl.program.create_with_source(
-        cl_ctx, @as([*]const []const u8, @ptrCast(&content))[0..1],
-        allocator
+        cl_ctx,
+        @as([*]const []const u8, @ptrCast(&content))[0..1],
+        allocator,
     );
-    defer cl.program.release(new_program) catch unreachable;
+    defer cl.program.release(new_program);
+
+    const type_index = w_tensor.getTypeId(T);
+    const vector_width = blk: {
+        if (options.vectors_enabled and !options.is_complex) {
+            break :blk command_queue.vector_widths[type_index];
+        }
+        break :blk 1;
+    };
 
     var args: []u8 = undefined;
-
-    const dtype = options.dtype;
-    const vector_width = if (options.vectors_enabled and !options.is_complex) command_queue.vector_widths[@intFromEnum(dtype)] else 1;
     if (options.extra_args) |v| {
         args = try std.fmt.allocPrint(
-            allocator, "-Dwk_width={d} -Ddtype={d} -Dmem_type={d} -Dcom={d} {s}\x00",
+            allocator,
+            "-Dwk_width={d} -Ddtype={d} -Dmem_type={d} -Dcom={d} {s}\x00",
             .{
                 vector_width,
-                @intFromEnum(dtype),
+                type_index,
                 command_queue.local_mem_type,
                 @intFromBool(options.is_complex),
-                v
-            }
+                v,
+            },
         );
-    }else{
+    } else {
         args = try std.fmt.allocPrint(
-            allocator, "-Dwk_width={d} -Ddtype={d} -Dmem_type={d} -Dcom={d}\x00",
+            allocator,
+            "-Dwk_width={d} -Ddtype={d} -Dmem_type={d} -Dcom={d}\x00",
             .{
                 vector_width,
-                @intFromEnum(dtype),
+                type_index,
                 command_queue.local_mem_type,
-                @intFromBool(options.is_complex)
-            }
+                @intFromBool(options.is_complex),
+            },
         );
     }
     defer allocator.free(args);
 
-    const header_prg = try compile_header(command_queue, dtype, options.vectors_enabled, options.is_complex, args);
+    const header_prg = try compile_header(
+        T,
+        command_queue,
+        options.vectors_enabled,
+        options.is_complex,
+    );
     const header_name: []const u8 = "wekua.h";
 
-    const devices: []const cl.device.cl_device_id = @as([*]const cl.device.cl_device_id, @ptrCast(&command_queue.device))[0..1];
+    const devices: []const cl.device.cl_device_id = @as(
+        [*]const cl.device.cl_device_id,
+        @ptrCast(&command_queue.device),
+    )[0..1];
+
     cl.program.compile(
-        allocator, new_program, devices, args,
+        allocator,
+        new_program,
+        devices,
+        args,
         @as([*]const cl.program.cl_program, @ptrCast(&header_prg))[0..1],
-        @as([*]const []const u8, @ptrCast(&header_name))[0..1], null, null
+        @as([*]const []const u8, @ptrCast(&header_name))[0..1],
+        null,
+        null,
     ) catch |err| {
         switch (err) {
             error.compile_program_failure => {
-                show_build_log(new_program, command_queue) catch unreachable;
+                show_build_log(new_program, command_queue) catch |err2| {
+                    std.log.err("Unexpected error while showing build log: {s}", .{@errorName(err2)});
+                    std.log.warn("No able to show build log", .{});
+                };
             },
-            else => {}
+            else => {},
         }
         return err;
     };
 
     program.* = cl.program.link(
-        cl_ctx, devices, null, @as([*]const cl.program.cl_program, @ptrCast(&new_program))[0..1],
-        null, null
+        cl_ctx,
+        devices,
+        null,
+        @as([*]const cl.program.cl_program, @ptrCast(&new_program))[0..1],
+        null,
+        null,
     ) catch |err| {
         switch (err) {
             error.link_program_failure => {
-                show_build_log(new_program, command_queue) catch unreachable;
+                show_build_log(new_program, command_queue) catch |err2| {
+                    std.log.err("Unexpected error while showing build log: {s}", .{@errorName(err2)});
+                    std.log.warn("No able to show build log", .{});
+                };
             },
-            else => {}
+            else => {},
         }
         return err;
     };
@@ -151,45 +199,42 @@ pub fn compile_kernel(
     kernel.* = try cl.kernel.create(program.*, options.kernel_name);
 }
 
-pub fn get_kernel_from_id(command_queue: wCommandQueue, kernel_id: wKernelsID) !wKernel {
-    var kernel = command_queue.kernels[@intFromEnum(kernel_id)];
-    if (kernel) |v| {
-        return v;
-    }
-
-    kernel = try command_queue.allocator.create(_w_kernel);
-    command_queue.kernels[@intFromEnum(kernel_id)] = kernel;
-    kernel.?.kernels = null;
-    kernel.?.programs = null;
-    return kernel.?;
-}
-
 pub fn get_kernel(
-    command_queue: wCommandQueue, kernel_id: wKernelsID, number_of_cl_kernels: usize
-) !wKernel {
-    const kernel = try get_kernel_from_id(command_queue, kernel_id);
-
-    if (kernel.kernels == null) {
-        const allocator = command_queue.allocator;
-
-        const kernels = try allocator.alloc(?cl.kernel.cl_kernel, number_of_cl_kernels);
-        errdefer allocator.free(kernels);
-        @memset(kernels, null);
-
-        const programs = try allocator.alloc(?cl.program.cl_program, number_of_cl_kernels);
-        @memset(programs, null);
-
-        kernel.kernels = kernels;
-        kernel.programs = programs;
+    command_queue: *const CommandQueue,
+    kernel_id: KernelsID,
+    comptime number_of_cl_kernels: usize,
+) !*KernelSet {
+    const kernels_set = &command_queue.kernels[@intFromEnum(kernel_id)];
+    if (kernels_set.initialized) {
+        return kernels_set;
     }
 
-    return kernel;
+    const allocator = command_queue.allocator;
+
+    const cl_kernels = try allocator.alloc(?cl.kernel.cl_kernel, number_of_cl_kernels);
+    errdefer allocator.free(cl_kernels);
+    @memset(cl_kernels, null);
+
+    const cl_programs = try allocator.alloc(?cl.program.cl_program, number_of_cl_kernels);
+    @memset(cl_programs, null);
+
+    kernels_set.kernels = cl_kernels;
+    kernels_set.programs = cl_programs;
+    kernels_set.initialized = true;
+
+    return kernels_set;
 }
 
 pub fn create_and_get_kernel(
-    command_queue: wCommandQueue, kernel_id: wKernelsID, kernel_source: []const u8, options: wCompileOptions,
-    comptime can_use_complex: bool, comptime can_use_vectors: bool, number_of_cl_kernels: usize,
-    getting_index_func: anytype, extra_args: anytype
+    comptime T: type,
+    command_queue: *const CommandQueue,
+    kernel_id: KernelsID,
+    kernel_source: []const u8,
+    options: CompileOptions,
+    comptime can_use_complex: bool,
+    comptime can_use_vectors: bool,
+    comptime number_of_cl_kernels: usize,
+    kernel_index: usize,
 ) !cl.kernel.cl_kernel {
     if (!can_use_complex and options.is_complex) {
         @panic("Kernels with complex numbers are not allowed");
@@ -199,99 +244,112 @@ pub fn create_and_get_kernel(
     }
 
     const kernels_set = try get_kernel(command_queue, kernel_id, number_of_cl_kernels);
-    const index: usize = getting_index_func(options.vectors_enabled, options.is_complex, options.dtype, extra_args);
-    if (kernels_set.kernels.?[index]) |kernel| {
+    if (kernels_set.kernels[kernel_index]) |kernel| {
         return kernel;
     }
 
     var kernel: cl.kernel.cl_kernel = undefined;
     var program: cl.program.cl_program = undefined;
 
-    try compile_kernel(
-        command_queue, options,
-        &kernel, &program,
-        kernel_source
-    );
+    try compile_kernel(T, command_queue, options, &kernel, &program, kernel_source);
 
-    kernels_set.kernels.?[index] = kernel;
-    kernels_set.programs.?[index] = program;
+    kernels_set.kernels[kernel_index] = kernel;
+    kernels_set.programs[kernel_index] = program;
 
     return kernel;
 }
 
-inline fn get_index_with_vectors_complex_and_dtype(
-    vectors_enabled: bool, is_complex: bool, dtype: wTensorDtype, _: anytype
-) usize {
-    return (
-        @intFromBool(vectors_enabled) * (2 * dtypes.number_of_dtypes) +
-        @intFromBool(is_complex) * dtypes.number_of_dtypes + @as(usize, @intFromEnum(dtype))
-    );
-}
-
 pub fn get_cl_kernel(
-    command_queue: wCommandQueue, tensor: wTensor, kernel_id: wKernelsID, kernel_name: []const u8,
-    kernel_source: []const u8, extra_args: ?[]const u8
+    comptime T: type,
+    command_queue: *const CommandQueue,
+    tensor: *const Tensor(T),
+    kernel_id: KernelsID,
+    kernel_name: []const u8,
+    kernel_source: []const u8,
+    extra_args: ?[]const u8,
 ) !cl.kernel.cl_kernel {
-    const dtype = tensor.dtype;
     const is_complex = tensor.is_complex;
+    const vectors_enabled = tensor.vectors_enabled;
+
+    const kernel_index = (@intFromBool(vectors_enabled) * (2 * w_tensor.SupportedTypes.len) +
+        @intFromBool(is_complex) * w_tensor.SupportedTypes.len + @as(usize, @intFromEnum(T)));
 
     return try create_and_get_kernel(
-        command_queue, kernel_id, kernel_source, .{
-            .dtype = dtype,
+        command_queue,
+        kernel_id,
+        kernel_source,
+        CompileOptions{
             .is_complex = is_complex,
-            .vectors_enabled = tensor.vectors_enabled,
+            .vectors_enabled = vectors_enabled,
             .kernel_name = kernel_name,
-            .extra_args = extra_args
-        }, true, true, dtypes.number_of_dtypes * 2 * 2, get_index_with_vectors_complex_and_dtype,
-        null
+            .extra_args = extra_args,
+        },
+        true,
+        true,
+        w_tensor.SupportedTypes.len * 2 * 2,
+        kernel_index,
     );
-}
-
-inline fn get_index_with_complex_and_dtype(
-    _: bool, is_complex: bool, dtype: wTensorDtype, _: anytype
-) usize {
-    return @intFromBool(is_complex) * dtypes.number_of_dtypes + @as(usize, @intFromEnum(dtype));
 }
 
 pub fn get_cl_no_vector_kernel(
-    command_queue: wCommandQueue, tensor: wTensor, kernel_id: wKernelsID, kernel_name: []const u8,
-    kernel_source: []const u8, extra_args: ?[]const u8
+    comptime T: type,
+    command_queue: *const CommandQueue,
+    tensor: *const Tensor(T),
+    kernel_id: KernelsID,
+    kernel_name: []const u8,
+    kernel_source: []const u8,
+    extra_args: ?[]const u8,
 ) !cl.kernel.cl_kernel {
     const dtype = tensor.dtype;
     const is_complex = tensor.is_complex;
 
+    const kernel_index = @intFromBool(is_complex) * w_tensor.SupportedTypes.len + @as(usize, @intFromEnum(T));
+
     return try create_and_get_kernel(
-        command_queue, kernel_id, kernel_source, .{
+        command_queue,
+        kernel_id,
+        kernel_source,
+        .{
             .dtype = dtype,
             .is_complex = is_complex,
             .vectors_enabled = false,
             .kernel_name = kernel_name,
-            .extra_args = extra_args
-        }, true, false, dtypes.number_of_dtypes * 2, get_index_with_complex_and_dtype,
-        null
+            .extra_args = extra_args,
+        },
+        true,
+        false,
+        w_tensor.SupportedTypes.len * 2,
+        kernel_index,
     );
 }
 
-inline fn get_index_with_dtype(
-    _: bool, _: bool, dtype: wTensorDtype, _: anytype
-) usize {
-    return @as(usize, @intFromEnum(dtype));
-}
-
 pub fn get_cl_no_vector_no_complex_single_kernel_per_dtype(
-    command_queue: wCommandQueue, tensor: wTensor, kernel_id: wKernelsID, kernel_name: []const u8,
-    kernel_source: []const u8, extra_args: ?[]const u8
+    comptime T: type,
+    command_queue: *const CommandQueue,
+    tensor: *const Tensor(T),
+    kernel_id: KernelsID,
+    kernel_name: []const u8,
+    kernel_source: []const u8,
+    extra_args: ?[]const u8,
 ) !cl.kernel.cl_kernel {
     const dtype = tensor.dtype;
 
     return try create_and_get_kernel(
-        command_queue, kernel_id, kernel_source, .{
+        command_queue,
+        kernel_id,
+        kernel_source,
+        .{
             .dtype = dtype,
             .is_complex = false,
             .vectors_enabled = false,
             .kernel_name = kernel_name,
-            .extra_args = extra_args
-        }, false, false, dtypes.number_of_dtypes, get_index_with_dtype,
-        null
+            .extra_args = extra_args,
+        },
+        false,
+        false,
+        w_tensor.SupportedTypes.len,
+        w_tensor.getTypeId(T),
     );
 }
+
+const KernelSet = @This();
