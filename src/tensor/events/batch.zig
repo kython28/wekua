9e1c304c323b9bf4399a36b14f6e0cf402824149
Enrique Mora <kike28.py@protonmail.ch>
2025-07-27 14:54:24 +0400

    feat: add OpenCL event management modules for tensor operations

---
+++ src/tensor/events/batch.zig
@@ -0,0 +1,158 @@
+const std = @import("std");
+const builtin = @import("builtin");
+
+const cl = @import("opencl");
+const Event = @import("event.zig");
+
+const BatchLenght = switch (builtin.mode) {
+    .Debug => 4,
+    .ReleaseSafe, .ReleaseFast => 128,
+    .ReleaseSmall => 16,
+};
+
+allocator: std.mem.Allocator,
+prev_events: ?[]cl.event.Event,
+
+events: [BatchLenght]Event,
+events_num: u8,
+
+pub fn init(
+    self: *Batch,
+    allocator: std.mem.Allocator,
+    prev_events: ?[]const cl.event.Event,
+) !void {
+    self.allocator = allocator;
+
+    if (prev_events) |pv| {
+        const _prev_events = try allocator.dupe(cl.event.Event, pv);
+        errdefer allocator.free(_prev_events);
+
+        var index: usize = 0;
+        errdefer {
+            for (pv[0..index]) |e| {
+                cl.event.release(e);
+            }
+        }
+
+        for (pv) |e| {
+            try cl.event.retain(e);
+            index += 1;
+        }
+
+        self.prev_events = _prev_events;
+    } else {
+        self.prev_events = null;
+    }
+
+    errdefer {
+        if (self.prev_events) |pv| {
+            for (pv) |e| {
+                cl.event.release(e);
+            }
+            allocator.free(pv);
+        }
+        self.prev_events = null;
+    }
+
+    for (&self.events, 0..) |*e, index| {
+        e.init(index, allocator);
+    }
+
+    self.events_num = 0;
+}
+
+pub inline fn empty(self: *const Batch) bool {
+    return (self.events_num == 0);
+}
+
+pub inline fn full(self: *const Batch) bool {
+    return (self.events_num == BatchLenght);
+}
+
+pub inline fn getPrevEvents(self: *const Batch) ?[]const cl.event.Event {
+    return self.prev_events;
+}
+
+pub inline fn clear(self: *Batch) void {
+    if (self.prev_events) |prev_events| {
+        for (prev_events) |e| {
+            cl.event.release(e);
+        }
+        self.allocator.free(prev_events);
+        self.prev_events = null;
+    }
+
+    for (self.events[0..self.events_num]) |*event| {
+        event.clear();
+    }
+}
+
+pub fn restart(self: *Batch, new_prev_events: ?[]const cl.event.Event) !void {
+    if (self.prev_events) |prev_events| {
+        for (prev_events) |e| {
+            cl.event.release(e);
+        }
+        self.allocator.free(prev_events);
+        self.prev_events = null;
+    }
+
+    if (new_prev_events) |pv| {
+        if (pv.len > Event.MaxEventsPerSet * 2) return error.EventsArrayTooLong;
+
+        var index: usize = 0;
+        errdefer {
+            for (pv[0..index]) |e| {
+                cl.event.release(e);
+            }
+        }
+
+        const prev_events = &self.prev_events;
+        while (index < pv.len) : (index += 1) {
+            const e = pv[index];
+
+            try cl.event.retain(e);
+            prev_events[index] = e;
+        }
+
+        self.prev_events_len = @intCast(pv.len);
+    }
+
+    for (self.events[0..self.events_num]) |*event| {
+        event.restart();
+    }
+
+    self.prev_events_len = 0;
+    self.events_num = 0;
+}
+
+pub fn waitForPendingEvents(self: *Batch) void {
+    var events_num = self.events_num;
+    if (events_num == 0) {
+        if (self.events[0].operation == .none) {
+            return;
+        }
+
+        events_num += 1;
+    } else if (events_num < BatchLenght) {
+        if (self.events[events_num].operation != .none) {
+            events_num += 1;
+        }
+    }
+    self.events_num = events_num;
+
+    for (self.events[0..events_num]) |*event| {
+        event.waitForEvents() catch |err| {
+            std.debug.panic(
+                "Unexpected error while waiting for events finalization: {s}",
+                .{@errorName(err)},
+            );
+        };
+    }
+}
+
+pub fn release(self: *Batch) void {
+    self.clear();
+    self.allocator.destroy(self);
+}
+
+const Batch = @This();
---
+++ src/tensor/events/event.zig
@@ -0,0 +1,144 @@
+const std = @import("std");
+const builtin = @import("builtin");
+
+const cl = @import("opencl");
+
+const Batch = @import("batch.zig");
+
+pub const AppendResult = enum { full, success_and_full, success };
+
+pub const UserCallback = struct {
+    func: *const fn (user_data: ?*anyopaque) void,
+    data: ?*anyopaque,
+};
+pub const UserCallbackArray = std.ArrayList(UserCallback);
+
+pub const Operation = enum {
+    write,
+    partial_write,
+    read,
+    none,
+};
+
+const MaxEventsPerSet = switch (builtin.mode) {
+    .Debug => 32,
+    .ReleaseFast, .ReleaseSafe => 256,
+    .ReleaseSmall => 64,
+};
+
+const MaxEventsPerSetInt = switch (builtin.mode) {
+    .Debug, .ReleaseSmall => u8,
+    .ReleaseFast, .ReleaseSafe => u16,
+};
+
+operation: Operation,
+
+callbacks: UserCallbackArray,
+events: [MaxEventsPerSet]cl.event.Event,
+events_count: MaxEventsPerSetInt,
+
+index: u8,
+
+pub fn init(self: *Event, index: usize, allocator: std.mem.Allocator) void {
+    self.operation = .none;
+    self.callbacks = UserCallbackArray.init(allocator);
+    self.events_count = 0;
+    self.index = @intCast(index);
+}
+
+pub inline fn getParent(self: *Event) *Batch {
+    const ptr: usize = @intFromPtr(self) - @offsetOf(Batch, "events") - @as(usize, self.index) * @sizeOf(Event);
+    return @ptrFromInt(ptr);
+}
+
+pub fn append(
+    self: *Event,
+    operation: Operation,
+    event: cl.event.Event,
+    user_callback: ?UserCallback,
+) !AppendResult {
+    const current_operation = self.operation;
+    const events_count = self.events_count;
+
+    if (current_operation == .none) {
+        self.operation = operation;
+    } else if (operation != current_operation or isFull(current_operation, events_count)) {
+        return AppendResult.full;
+    }
+    errdefer {
+        if (current_operation == .none) {
+            self.operation = .none;
+        }
+    }
+
+    if (user_callback) |callback| {
+        try self.callbacks.append(callback);
+    }
+
+    self.events[events_count] = event;
+    self.events_count = events_count + 1;
+
+    if (isFull(operation, events_count + 1)) return AppendResult.success_and_full;
+
+    return AppendResult.success;
+}
+
+pub inline fn pop(self: *Event, had_callback: bool) void {
+    const events_count = self.events_count;
+    self.events_count = events_count - 1;
+    if (events_count == 0) {
+        self.operation = .none;
+    }
+
+    if (had_callback) {
+        _ = self.callbacks.pop();
+    }
+}
+
+pub inline fn isFull(operation: Operation, events_count: usize) bool {
+    return switch (operation) {
+        .read, .partial_write => (events_count == MaxEventsPerSet),
+        .write => (events_count == 1),
+        .none => false,
+    };
+}
+
+pub inline fn full(self: *const Event) bool {
+    return isFull(self.operation, self.events_count);
+}
+
+pub inline fn toSlice(self: *const Event) []const cl.event.Event {
+    return self.events[0..self.events_count];
+}
+
+pub inline fn appendCallback(self: *Event, callback: UserCallback) !void {
+    try self.callbacks.append(callback);
+}
+
+pub inline fn executeCallbacks(self: *const Event) void {
+    for (self.callbacks.items) |*callback| {
+        callback.func(callback.data);
+    }
+}
+
+pub inline fn waitForEvents(self: *const Event) !void {
+    try cl.event.waitForMany(self.toSlice());
+    self.executeCallbacks();
+}
+
+pub inline fn clear(self: *Event) void {
+    for (self.events[0..self.events_count]) |event| {
+        cl.event.release(event);
+    }
+
+    self.callbacks.deinit();
+}
+
+pub inline fn restart(self: *Event) void {
+    self.clear();
+
+    self.operation = .none;
+    self.events_count = 0;
+}
+
+const Event = @This();
